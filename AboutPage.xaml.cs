4cbe26133f2652c93398fb752ff3c55d36cbc89c
Adrian Soim <adisoim2002@gmail.com>
2023-11-20 18:09:52 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Soim_Adrian_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Soim_Adrian_Lab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
